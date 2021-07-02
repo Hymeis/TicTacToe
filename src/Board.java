67a4fe64cf25b24b703ac457246fe5e72df6cc8c
Timothy C <44147445+Hymeis@users.noreply.github.com>
2021-07-02 15:06:50 -0700

    Initial Structure

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,23 @@
 public class Board {
+    private int[][] board = new int[3][3];
+    private final int X = 1;
+    private final int O = 2;
+    private static int step = 0;
+    public Board(){
+
+    }
+
+    private void start(){
+        while(true){
+
+        }
+    }
+
+    private boolean win(){
+
+    }
+
+    private void restart(){
+
+    }
 }
