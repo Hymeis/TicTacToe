ce3717a3cf7323365dee3c52e89595bc6ff27208
Andy Qin <56485866+AndyQYH@users.noreply.github.com>
2021-07-03 20:02:04 -0700

    Please work

--- src/Board.java
+++ src/Board.java
@@ -18,6 +18,13 @@ public class Board extends JPanel {
     public static final int SCREEN_LENGTH = 1000;
 
 
+public class Board {
+    private int[][] board = new int[3][3];//tictac board = 3 x 3 2D array
+    private final int X = 0;              //X = 1st PLayer(goes first)
+    private final int O = 1;              //O = 2nd Player(goes second)
+    private static int step = 0;          //step = total number of steps played by both players
+
+    //constrcutor of Board class
     public Board(){
         init();
         //initializeButtons();
@@ -28,7 +35,15 @@ public class Board extends JPanel {
         int k = -1;
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
+<<<<<<< HEAD
                 board[i][j] = ""+ k--;
+=======
+<<<<<<< HEAD
+                board[i][j] = -1;         //initialize each spot of the board to be -1 = spot is available
+=======
+                board[i][j] = k--;
+>>>>>>> cef7950fbe216a3ee69e7e037ced371ae1c05d56
+>>>>>>> da8333e2a8488ba94e6a8be2546b0d15862e2939
             }
         }
         step = 0;
@@ -94,6 +109,7 @@ public class Board extends JPanel {
     }
 
     private void place(int i, int j){
+<<<<<<< HEAD
         if (Integer.parseInt(board[i][j]) < 0){
             board[i][j] = (step % 2 == 0) ? " X" : " O";
             step++;
@@ -130,6 +146,30 @@ public class Board extends JPanel {
                     System.out.println("Input too short");
                 }catch (IllegalAccessException e) {
                     System.out.println("The spot is already taken, please try another spot!");
+=======
+        if (board[i][j] != -1){
+            board[i][j] = (step % 2 == 0) ? X : O;//if player move is even, it is the first player's turn; else the second player
+            step++;                       //everytime place() is called, increment total step played
+        }
+    }
+
+<<<<<<< HEAD
+    //initialize game
+    private void start(){
+=======
+    public void start(){
+>>>>>>> cef7950fbe216a3ee69e7e037ced371ae1c05d56
+        while(true){
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter [Row] + [ ] + [Column]");
+            String out = myObj.nextLine();
+            int row = -1, column= -1;
+            try{
+                row = Integer.parseInt(out.substring(0,1));
+                column = Integer.parseInt(out.substring(2,3));
+                if (row < 0 || row > 2 || column < 0 || column > 2) {
+                    throw new NumberFormatException();
+>>>>>>> da8333e2a8488ba94e6a8be2546b0d15862e2939
                 }
 
                 //System.out.println("Column: " + column);
@@ -150,9 +190,23 @@ public class Board extends JPanel {
             }
         }
     }
+<<<<<<< HEAD
 
     //checks if any player wins
+=======
+    
+    //
+>>>>>>> da8333e2a8488ba94e6a8be2546b0d15862e2939
     private boolean check(){
+<<<<<<< HEAD
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter [Row] + [ ] + [Column]");
+        String out = myObj.nextLine();   
+        try{
+
+        } catch (Exception e){
+
+=======
         String winner = (step % 2 == 1) ? "X" : "O";
         for (int i = 0; i < 3; i++){
             if (board[0][i] == board[1][i] && board[1][i] == board[2][i]){ // horizontal
@@ -170,6 +224,7 @@ public class Board extends JPanel {
                 || board[0][2] == board[1][1] && board[1][1] == board[2][0]){ // diagonal
             System.out.println(winner + " wins");
             return true;
+>>>>>>> cef7950fbe216a3ee69e7e037ced371ae1c05d56
         }
 
         if(step >= board.length * board[0].length)
